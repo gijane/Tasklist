5fcf45a5da6b1ca6fa538dd19a1b693b53337b25
Peter Short <peter.short@gmail.com>
2016-03-27 16:17:26 +0000

    updated mock object

--- mockTasksResource.js
+++ mockTasksResource.js
@@ -1,18 +1,54 @@
 
+// callback is a function that is of the format 
+// 		function(result, error) {
+// 		}
+//
+
 module.exports = {
 
+	var tasks = [
+		{
+			id: 1,
+			taskname: 'Task1',
+			description: 'The first task',
+		},
+		{
+			id: 2,
+			taskname: 'Task2',
+			description: 'The second task',
+		},
+		{
+			id: 3,
+			taskname: 'Task3',
+			description: 'The third task',
+		}
+	];
+
 	getTasks: function(callback) {
+
+		callback(tasks, null);
+
 	},
 
 	getSingleTask: function(id, callback) {
+
+		if(id < tasks.length) {
+			callback(tasks[id], null);
+		} else {
+			callback(null, { message: 'There is no task with that index in the mock' });
+		}
+		
 	},
 
 	createTask: function(task, callback) {
+
 	},
 
 	updateTask: function(task, callback) {
+
 	},
 
 	deleteTask: function(id, callback) {
+
 	}
 };
