b069ab3de35e2f964da11d097e4b7fefdb40de65
Peter Short <peter.short@gmail.com>
2016-04-01 21:30:26 +0000

    put in skeleton for resource

---
+++ resources/postgresTasksResource.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+
+	getTasks: function(callback) {
+	},
+
+	getSingleTask: function(id, callback) {
+	},
+
+	createTask: function(task, callback) {
+	},
+
+	updateTask: function(task, callback) {
+	},
+
+	deleteTask: function(id, callback) {
+	}
+}
