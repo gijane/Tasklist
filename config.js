ce1a14564c183bdb7b8d44ebe9261d154d789d8a
Peter Short <peter.short@gmail.com>
2016-04-02 16:28:14 +0000

    added database connection string

---
+++ config.js
@@ -0,0 +1,5 @@
+var pg_string = 'postgres://gmdjxoxgvvmvfi:pLNAbpvnegxubbzWWTrDLYVLEd@ec2-54-83-51-0.compute-1.amazonaws.com:5432/d155l2gs35p7ak?ssl=true';
+
+module.exports = {
+   dbConnection: pg_string
+};
