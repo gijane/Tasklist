d075b39fbd8ecfc412f4f7145d6d1001537f0223
Peter Short <peter.short@gmail.com>
2016-04-02 16:26:22 +0000

    added sql that creates the database

---
+++ taskList.sql
@@ -0,0 +1,16 @@
+drop table if exists tl.tasks;
+drop schema if exists tl;
+
+create schema tl;
+
+create table tl.tasks (
+	id			SERIAL			PRIMARY KEY,
+	name		varchar(100)	NOT NULL,
+	description	varchar(6000),
+	created		timestamp		DEFAULT current_timestamp,
+	due			timestamp
+);
+
+insert into tl.tasks (name, description) values ('Task 1', 'The first task, its a good one?');
+
+SELECT * FROM tl.tasks;
