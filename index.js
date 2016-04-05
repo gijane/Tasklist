599443504747d9611fc6423ca93c89ed02a3b7f8
Peter Short <peter.short@gmail.com>
2016-04-05 22:10:56 +0000

    added cors

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var config = require('./config');
-
+var cors = require('cors');
 
 //var resource = require('./resources/mockTasksResource');
 var resource = require('./resources/postgresTasksResource')(config);
 
 var api = require('./api')(resource);
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
